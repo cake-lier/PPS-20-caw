cd2121909072066eec1de8ce4d69891aea9df0af
Yuqi Sun <yuqi.sun@studio.unibo.it>
2021-09-09 15:34:50 +0200

    Rename Component into ViewComponent in MainMenu

--- src/main/scala/view/MainMenu.scala
+++ src/main/scala/view/MainMenu.scala
@@ -8,12 +8,12 @@ import javafx.scene.control.{Button, Label}
 
 /** Factory for new [[MainMenu]] instance. */
 object MainMenu {
+
   /** Creates a main menu component. */
-  def apply(): Component[Pane] = new MainMenuImpl()
+  def apply(): ViewComponent[Pane] = new MainMenuImpl()
 
   /** Implementation of the MainMenu. */
-  private final class MainMenuImpl
-      extends AbstractViewComponent[Pane]("main_menu_page.fxml") {
+  private final class MainMenuImpl extends AbstractViewComponent[Pane]("main_menu_page.fxml") {
     @FXML
     var playButton: Button = _
     @FXML
