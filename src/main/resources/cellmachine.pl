feb25d5fbe1a128fe7d8f89761ee6813f28dec33
Matteo Castellucci <matteino98@gmail.com>
2021-09-17 18:27:04 +0200

    Make PROLOG code more coherent between cell types predicates

--- src/main/resources/cellmachine.pl
+++ src/main/resources/cellmachine.pl
@@ -1,45 +1,69 @@
-% is_movable_right(Machine, Type, XCoordinate, YCoordinate)
+% is_movable_right(Board, Type, XCoordinate, YCoordinate)
 % 
-% This predicate checks whether or not a given machine contains cells that can be moved as a whole to the right by one position. 
-% The check starts from the given initial cell and then recursively moves along the machine from left to right on the same row of 
-% the previous cell. This means that getting the next cell is equivalent to getting the one adjacent to the right of the previous.
-% Any given cell can move to the right if it is one that can always move to the right (an empty cell or an enemy cell) or if it is
-% a cell that can move to the right if it is adjacent to a cell that can move to the right, which is any cell except the the wall
-% cell and the vertical block cell (which can never be moved in the right direction) and the generator left and the arrow left
-% cell (because they move in a direction opposite to the right one).
-is_movable_right(_, enemy, _, _).
-is_movable_right(M, T, X, Y) :- T \= wall,
-                                T \= block_ver,
-                                T \= generator_left,
-                                T \= arrow_left,
-                                X1 is X + 1,
-                                (member(cell(T1, X1, Y), M) -> is_movable_right(M, T1, X1, Y); true).
+% This predicate checks whether or not a board contains a row of cells that can be moved to the right by one position starting
+% from the cell which coordinates and type are given. The check starts from the initial cell and then recursively moves along the
+% board from left to right on the same row of the previous cell. This means that getting the next cell is equivalent to getting
+% the one adjacent to the right of the previous one. Any given cell can move to the right if it is one that can always move to the
+% right, like an "enemy" cell, or if it is a cell that can move to the right if it is adjacent to a cell that can move to the
+% right, which is any cell except the the "wall" cell and the "vertical block" cell (which can never be moved in the right
+% direction) and the "generator left" and the "arrow left" cell (because they move in a direction opposite to the right one). If
+% no other cell is found while traversing the board as previously described, it is assumed that an empty cell is met, which can
+% always be moved to the right.
+last_index_right(_, enemy, X, _, X) :- !.
+last_index_right(B, T, X, Y, EX) :- T \= wall,
+                                    T \= block_ver,
+                                    T \= arrow_left,
+                                    X1 is X + 1,
+                                    (member(cell(T1, X1, Y), B) -> last_index_right(B, T1, X1, Y, EX); EX = X).
 
-% move_right(Machine, XCoordinate, YCoordinate, NextMachine)
+% move_right(Board, XCoordinate, YCoordinate, NextBoard)
 % 
-% This predicate shifts all cells in the machine to the right by one position except the enemy cells and the empty cells, which
-% are deleted.
-move_right([], _, _, []).
-move_right([cell(enemy, X, Y) | CS], X, Y, CS).
-move_right([cell(T, X, Y) | CS], X1, Y1, [cell(T, X, Y) | NCS]) :- (X =\= X1; Y =\= Y1), move_right(CS, X1, Y1, NCS), !.
-move_right([cell(_, X, Y) | CS], X, Y, NM) :- X1 is X + 1, member(cell(enemy, X1, Y), CS), move_right(CS, X1, Y, NM), !.
-move_right([cell(T, X, Y) | CS], X, Y, [cell(T, X1, Y) | NCS]) :- X1 is X + 1, move_right(CS, X1, Y, NCS).
+% This predicate applies the rule for the "arrow right" cell behavior. This predicate considers only the cells which are adjacent
+% to the right of the "arrow right" cell which coordinates are given. If, while moving left to right from the "arrow right" cell,
+% an "enemy" cell is found, then the remaining cells are ignored by this predicate. This row of cells is then moved one position
+% to the right. If the last cell of the row is an enemy cell, the cell is destroyed as is destroyed the one previous to the enemy
+% cell. Those two cells will not be present in the next board state. No ordering is required for the cells in the board and the
+% empty cells must not be present.
+move_right([], _, _, _, _, []).
+move_right([cell(enemy, EX, Y) | CS], B, SX, EX, Y, NB) :- move_right(CS, B, SX, EX, Y, NB), !.
+move_right([cell(T, X, Y) | CS], B, SX, EX, Y1, [cell(T, X, Y) | NCS]) :- ((T = generator_right, X =:= SX, Y =:= Y1);
+                                                                           X < SX;
+                                                                           X > EX + 1;
+                                                                           Y =\= Y1),
+                                                                          move_right(CS, B, SX, EX, Y1, NCS), 
+                                                                          !.
+move_right([cell(_, X, Y) | CS], B, SX, EX, Y, NB) :- X >= SX, 
+                                                      X < EX,
+                                                      X1 is X + 1,
+                                                      member(cell(enemy, X1, Y), B),
+                                                      move_right(CS, B, SX, EX, Y, NB),
+                                                      !.
+move_right([cell(T, X, Y) | CS], B, SX, EX, Y, [cell(T, X1, Y)| NCS]) :- X >= SX, 
+                                                                         X =< EX,
+                                                                         X1 is X + 1,
+                                                                         move_right(CS, B, SX, EX, Y, NCS).
 
-% arrow_right_next_state(Machine, XCoordinate, YCoordinate, NextMachine)
+% arrow_right_next_state(Board, XCoordinate, YCoordinate, NextBoard)
 % 
-% This predicate allows to update the state of a machine (a list of cells on a board) and obtain its next state when the first
-% cell is a "arrow right" cell. Hence, this predicate is for applying the "arrow right" rule. The cells that makes its
-% "surroundings" and needs to be passed with the first cell in the machine are all cells which are to the right of the first cell.
-% These cells need to be inserted into the machine until an empty cell, an enemy cell or a wall cell is met while moving left to
-% right from the first arrow cell. If the row of cells is deemed to be movable to the right, all cells are then moved and the last
-% one deleted. This is because, in this case, the last cell can only be an enemy cell or an empty cell. The order of the cells in
-% the list can not respect the order of the cells in the board.
-arrow_right_next_state(M, X, Y, NM) :- member(cell(arrow_right, X, Y), M),
-                                       is_movable_right(M, arrow_right, X, Y),
-                                       move_right(M, X, Y, NM),
+% This predicate allows to update the state of a board and obtain its next state applying the rule for the "arrow right" cell
+% behavior. This predicate considers only the cells which are adjacent to the right of the "arrow right" cell which coordinates
+% are given. If, while moving left to right from the "arrow right" cell, an "enemy" cell is found, then the remaining cells are
+% ignored by this predicate. If the row of cells built as previously described is deemed to be movable to the right by one
+% position, the row is then moved. If the last cell of the row is an enemy cell, the cell is destroyed as is destroyed the one
+% previous to the enemy cell. Those two cells will not be present in the next board state. If the coordinates does not point to an
+% "arrow right" cell, the predicate simply evaluates to "no". No ordering is required for the cells in the board and the empty
+% cells must not be present.
+arrow_right_next_state(B, X, Y, NB) :- member(cell(arrow_right, X, Y), B),
+                                       last_index_right(B, arrow_right, X, Y, EX),
+                                       move_right(B, B, X, EX, Y, NB),
                                        !.
-arrow_right_next_state(M, _, _, M).
+arrow_right_next_state(B, _, _, B).
 
+% rotate_right(Board, XCoordinate, YCoordinate, NextBoard)
+% 
+% This predicate applies the rule for the "rotate right" cell behavior. The rule rotate in a clockwise direction the cells which
+% are orthogonally adjacent to the "rotate cell" which coordinates are given. If they can not be rotated or are not orthogonally
+% adjacent, the cells are left as they were.
 rotate_right([], _, _, []).
 rotate_right([cell(T, X1, Y1) | CS], X, Y, [cell(T1, X1, Y1) | NCS]) :- ((X1 is X + 1, Y1 =:= Y);
                                                                          (X1 =:= X, Y1 is Y + 1);
@@ -59,49 +83,19 @@ rotate_right([cell(T, X1, Y1) | CS], X, Y, [cell(T1, X1, Y1) | NCS]) :- ((X1 is 
                                                                         !.
 rotate_right([cell(T, X1, Y1) | CS], X, Y, [cell(T, X1, Y1) | NCS]) :- rotate_right(CS, X, Y, NCS).
 
-% rotate_right_next_state(Machine, XCoordinate, YCoordinate, NextMachine)
+% rotate_right_next_state(Board, XCoordinate, YCoordinate, NextBoard)
 % 
-% This predicate allows to update the state of a machine (a list of cells on a board) and obtain its next state when the first
-% cell is a "rotate right" cell. Hence, this predicate is for applying the "rotate right" rule. The cells that makes its
-% "surroundings" and needs to be passed with the first cell in the machine are its orthogonally adjacents cells. Then each cell,
-% except for the first, rotate cell, is then rotated clockwise if it can be rotated. If it can not, is then leaved as it is. No
-% translation of coordinates is applied by this rule.
-rotate_right_next_state(M, X, Y, NM) :- member(cell(rotate_right, X, Y), M), !, rotate_right(M, X, Y, NM).
+% This predicate allows to update the state of a board and obtain its next state applying the rule for the "rotate right" cell
+% behavior. The rule rotate in a clockwise direction the cells which are orthogonally adjacent to the "rotate cell" which
+% coordinates are given. If they can not be rotated or are not orthogonally adjacent, the cells are left as they were. If the
+% coordinates does not point to a "rotate right" cell, the predicate simply evaluates to "no". No ordering is required for the
+% cells in the board and the empty cells must not be present.
+rotate_right_next_state(B, X, Y, NB) :- member(cell(rotate_right, X, Y), B), !, rotate_right(B, X, Y, NB).
 
-
-%-----------------------------------------------------------------------------------------------------------------------------
-% get_furthest_right(+Machine, +XCoordinate, +YCoordinate, +TemporaryFurthestRightCell, -FurthestRightCell)
-% This predicate return the furthest right cell of the generator_right or of the already generated cells.
-get_furthest_right(M, X, Y, TMP, NC) :- X1 is X + 1,
-                                        member(cell(T, X1, Y), M),
-                                        get_furthest_right(M, X1, Y, cell(T, X1, Y), NC), !.
-get_furthest_right(M, X, Y, TMP, NC) :- NC = TMP.
-
-% generate_right(+Machine, +Type, +FurthestRightCell, -NextMachine)
-% This predicate adds a new cell to the machine only if the furthest right cell is the same type specified in input - that is the
-% generator_right has already generated some cells and there is still a non-occupied cell in its right - or is a generator_right
-% - that is the generator_right has not generated any cells yet. If the furthest right cell is a wall, a block_ver, an arrow_left
-% the cell generation is blocked; if it's an enemy, this will be destroyed and removed from the machine.
-generate_right([], _, _, []):- !.
-generate_right([cell(enemy, X, Y) | CS], _, cell(enemy, X, Y), CS):- !.
-generate_right([cell(T, X, Y) | CS], _, cell(T, X, Y), [cell(T, X, Y)|CS]) :- (T = wall; T = block_ver; T = arrow_left), !.
-generate_right([cell(T, X, Y) | CS], T2, cell(T1, X1, Y1), [cell(T, X, Y) | NCS]) :-
-                                                                              (X \= X1; Y \= Y1),
-                                                                               generate_right(CS, T2, cell(T1, X1, Y1), NCS), !.
-generate_right([cell(T, X, Y) | CS], T1, cell(T, X, Y), [cell(T, X, Y), cell(T1, X1, Y)| NCS]) :-
-                                                                               (T = T1; T = generator_right),
-                                                                                X1 is X + 1,
-                                                                                generate_right(CS, T, cell(T, X, Y), NCS), !.
-
-% generator_right_next_state(+Machine, XCoordinate, YCoordinate, NextMachine)
-% This predicate applys the "right generation" rule: it takes the previous state of the machine and returns its new state. The
-% rule is applied only if there is cell placed at the left of the generator_right and there is a non-occupied cell at the right
-% of the generator_right or at the right of the already generated cells: if these conditions are true, the predicate will return a
-% new machine containing a new generated cell, otherwise it will return the old machine. 
-generator_right_next_state(M, X, Y, NM) :- member(cell(generator_right, X, Y), M),
+generator_right_next_state(B, X, Y, NB) :- member(cell(generator_right, X, Y), B),
                                            X1 is X - 1,
-                                           member(cell(T, X1, Y), M),
-                                           get_furthest_right(M, X, Y, cell(generator_right, X, Y), NC),
-                                           generate_right(M, T, NC, NM),
+                                           member(cell(T, X1, Y), B),
+                                           last_index_right(B, generator_right, X, Y, EX),
+                                           move_right([cell(T, X, Y) | B], B, X, EX, Y, NB),
                                            !.
 generator_right_next_state(M, _, _, M).
