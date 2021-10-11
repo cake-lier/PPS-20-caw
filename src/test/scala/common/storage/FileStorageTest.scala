dbe1dccd789c75a722f650e10be1bebf0c882e4c
Elena Rughi <elena.rughi2@studio.unibo.it>
2021-10-11 15:07:45 +0200

    Add tests for FileStorage

---
+++ src/test/scala/common/storage/FileStorageTest.scala
@@ -0,0 +1,72 @@
+package it.unibo.pps.caw.common.storage
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+import java.io.{File, FileNotFoundException}
+import java.nio.file.{Files, InvalidPathException, OpenOption, Paths, StandardOpenOption}
+import java.time.LocalDateTime
+import scala.io.Source
+import scala.util.{Failure, Success, Using}
+
+class FileStorageTest extends AnyFunSpec with Matchers {
+  private val fileStorage = FileStorage()
+  private val level01 =
+    "{   \"width\": 8,   \"height\": 5,   \"playableArea\": {     \"width\": 4,     \"height\": 5,     \"x\": 0,     \"y\": 0   },   \"cells\": {     \"mover\": [       {         \"orientation\": \"right\",         \"x\": 1,         \"y\": 1       }     ],     \"enemy\": [       {         \"x\": 6,         \"y\": 3       }     ]   } }"
+
+  describe("FileStorage") {
+    describe("when asked to load a resource") {
+      it("should correctly load the resource") {
+        fileStorage.loadResource("level01.json").get shouldBe level01
+      }
+      describe("if the resource does not exist") {
+        it("should produce a FileNotFoundException") {
+          fileStorage.loadResource("nonexistant") match {
+            case Failure(e: FileNotFoundException) => succeed
+            case _                                 => fail()
+          }
+        }
+      }
+    }
+
+    describe("when asked to load a file") {
+      it("should correctly load the file") {
+        val testString = "Testing file loading."
+        val path = System.getProperty("user.home") + File.separator + "fileStorageTesting"
+        Files.writeString(Paths.get(path), testString)
+        fileStorage.loadFile(path).get shouldBe testString
+        Files.deleteIfExists(Paths.get(path))
+      }
+      describe("if the file does not exist") {
+        it("should produce a FileNotFoundException") {
+          val path = System.getProperty("user.home") + File.separator + "nonexistant"
+          fileStorage.loadFile(path) match {
+            case Failure(e: FileNotFoundException) => succeed
+            case _                                 => fail()
+          }
+        }
+      }
+    }
+
+    describe("when asked to write a file") {
+      it("should correctly write a file") {
+        val testString = "Testing file writing."
+        val path = System.getProperty("user.home") + File.separator + "fileStorageTesting"
+        fileStorage.writeFile(path, testString)
+        Using(Source.fromFile(path))(_.getLines.mkString).get shouldBe testString
+        Files.deleteIfExists(Paths.get(path))
+      }
+
+      describe("if the file path is not valid") {
+        it("should produce an InvalidPathException") {
+          val path = System.getProperty("user.home") + "?" + "fileStorageTesting"
+          fileStorage.writeFile(path, "test") match {
+            case Failure(e: InvalidPathException) => succeed
+            case _                                => fail()
+          }
+        }
+      }
+    }
+  }
+
+}
